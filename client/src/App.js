aeadf9be2e7c297cccf20a92c70cfef3dad8dbcf
CoderGuy614 <jlutz.110@gmail.com>
2020-05-07 20:39:46 +0800

    added product table and products components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from "react";
-import { UserForm } from "./components/UserForm";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { UserForm } from "./components/stepForm/UserForm";
+import Products from "./components/Products";
+import Navbar from "./components/Navbar";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <UserForm />
-      </div>
+      <Fragment>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Products} />
+            <Route exact path="/checkout" component={UserForm} />
+          </Switch>
+        </BrowserRouter>
+      </Fragment>
     );
   }
 }
---
+++ client/src/components/ProductTable.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const TAX_RATE = 0.07;
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 375,
+  },
+});
+
+function ccyFormat(num) {
+  return `${num.toFixed(2)}`;
+}
+
+function priceRow(qty, unit) {
+  return qty * unit;
+}
+
+function createRow(desc, qty, unit, pricePer) {
+  const price = priceRow(qty, unit);
+  return { desc, qty, unit, price, pricePer };
+}
+
+function subtotal(items) {
+  return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
+}
+
+export default function SpanningTable({ items }) {
+  const rows = items.map((item) =>
+    createRow(item.name, 0, item.price, item.pricePer)
+  );
+  const classes = useStyles();
+  const invoiceSubtotal = subtotal(rows);
+  const invoiceTaxes = TAX_RATE * invoiceSubtotal;
+  const invoiceTotal = invoiceTaxes + invoiceSubtotal;
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="spanning table">
+        <TableHead>
+          {/* <TableRow>
+            <TableCell align="center" colSpan={3}>
+              Details
+            </TableCell>
+            <TableCell align="right">Price</TableCell>
+          </TableRow> */}
+          <TableRow>
+            <TableCell>Item</TableCell>
+            <TableCell align="right">Qty.</TableCell>
+            <TableCell align="right">Unit</TableCell>
+            <TableCell align="right">Per</TableCell>
+            <TableCell align="right">Sum</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.desc}>
+              <TableCell>{row.desc}</TableCell>
+              <TableCell align="right">{row.qty}</TableCell>
+              <TableCell align="right">{row.unit}</TableCell>
+              <TableCell align="right">{row.pricePer}</TableCell>
+              <TableCell align="right">{ccyFormat(row.price)}</TableCell>
+            </TableRow>
+          ))}
+
+          <TableRow>
+            <TableCell rowSpan={3} />
+            <TableCell colSpan={2}>Subtotal</TableCell>
+            <TableCell align="right">{ccyFormat(invoiceSubtotal)}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Tax</TableCell>
+            <TableCell align="right">{`${(TAX_RATE * 100).toFixed(
+              0
+            )} %`}</TableCell>
+            <TableCell align="right">{ccyFormat(invoiceTaxes)}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell colSpan={2}>Total</TableCell>
+            <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
