7e87e4f6a4e82595e1b9bf6d05d3770bd4034c53
CoderGuy614 <jlutz.110@gmail.com>
2020-05-29 11:49:59 +0800

    prepare for deploy

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ app.use("/api/orders", require("./routes/api/orders"));
 app.use("/api/messages", require("./routes/api/messages"));
 app.use("/api/confirmation", require("./routes/api/confirmation"));
 
+// Server Static Assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set Static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server is running on ${PORT}`));
